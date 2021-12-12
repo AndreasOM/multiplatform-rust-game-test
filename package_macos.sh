47c2f1fb0c029a0ebe25d20de137929067b72676
Andreas Neukoetter <andreas@omni-mad.com>
2021-12-12 19:11:34 +0100

    : Fixup.

--- package_macos.sh
+++ package_macos.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+binary=game-rs
+binary_dir=game-rs
+
 platform=macos
 app_dir=${platform}/game-rs.app
 
@@ -10,10 +13,12 @@ mkdir -p ${app_dir}/Contents/MacOS	# save a few mkdir calls by starting with a d
 mkdir -p ${app_dir}/Contents/Resources
 
 echo "Combining binaries int fat binaries"
-lipo -create -output ${app_dir}/Contents/MacOS/fiiish target/aarch64-apple-darwin/release/fiiish target/x86_64-apple-darwin/release/fiiish
+lipo -create -output ${app_dir}/Contents/MacOS/${binary} \
+	${binary_dir}/target/aarch64-apple-darwin/release/${binary} \
+	${binary_dir}/target/x86_64-apple-darwin/release/${binary}
 
 build_number=$(git rev-list --all --count)
 echo "Build number: ${build_number}"
 
-version=$(cargo get version)
-echo "Version: ${build_number}"
+version=$(cd ${binary_dir} && cargo get version)
+echo "Version: ${version}"

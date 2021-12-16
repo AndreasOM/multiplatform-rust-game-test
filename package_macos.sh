abd9b8ab4bb46c2cbfb8e91523473f1bca3a036f
Andreas Neukoetter <andreas@omni-mad.com>
2021-12-16 17:54:16 +0100

    + Add version to folder name.

--- package_macos.sh
+++ package_macos.sh
@@ -4,7 +4,18 @@ binary=game-rs
 binary_dir=game-rs
 
 platform=macos
-app_dir=${platform}/game-rs.app
+
+
+build_number=$(git rev-list --all --count)
+echo "Build number: ${build_number}"
+
+# TODO: make this check if cargo get is available
+# faster than installing cargo-get:
+version=$(grep version ${binary_dir}/Cargo.toml|cut -d"\"" -f2)
+# version=$(cd ${binary_dir} && cargo get version)
+echo "Version: ${version}"
+
+app_dir=package/${platform}-${version}/game-rs.app
 
 echo "Creating folder structure in ${app_dir}"
 mkdir -p ${app_dir}
@@ -17,11 +28,3 @@ lipo -create -output ${app_dir}/Contents/MacOS/${binary} \
 	${binary_dir}/target/aarch64-apple-darwin/release/${binary} \
 	${binary_dir}/target/x86_64-apple-darwin/release/${binary}
 
-build_number=$(git rev-list --all --count)
-echo "Build number: ${build_number}"
-
-# TODO: make this check if cargo get is available
-# faster than installing cargo-get:
-version=$(grep version ${binary_dir}/Cargo.toml|cut -d"\"" -f2)
-# version=$(cd ${binary_dir} && cargo get version)
-echo "Version: ${version}"
